fabea534b6eece3a1a6259e3f72a2a70688c342d
Me <clairempr@users.noreply.github.com>
2018-11-25 22:21:51 -0500

    Make sure setting of Employee.vrc happens in admin

--- bureau/personnel/admin.py
+++ bureau/personnel/admin.py
@@ -6,6 +6,13 @@ class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'bureau_state', 'vrc')
     list_filter = ('bureau_states', 'vrc', 'regiments')
 
+    def save_model(self, request, obj, form, change):
+        # Make sure someone who's a member of a VRC regiment has VRC set to true
+        if form.cleaned_data['regiments'].filter(vrc=True):
+            obj.vrc = True
+
+        super(EmployeeAdmin, self).save_model(request, obj, form, change)
+
     def bureau_state(self, obj):
         return obj.bureau_state_list()
 
