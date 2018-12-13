4da12f22ad6af645fc6a1335971b8f29e594dddc
clairempr <clairempr@users.noreply.github.com>
2018-12-12 22:19:19 -0500

    Unit test for Regiment.__str__, remove relative imports from unit tests

---
+++ bureau/military/tests/test_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from military.tests.factories import RegimentFactory
+
+
+class RegimentTestCase(TestCase):
+    """
+    Test Regiment model
+    """
+
+    def test_str(self):
+        """
+        If __str__ should return Regiment.name
+        """
+
+        regiment = RegimentFactory(name='3rd Maine Volunteer Infantry Regiment')
+        self.assertEqual(str(regiment), regiment.name,
+                        "Regiment.__str__ should be equal to Regiment.name")
