4aa70f4c81f49923c5a06250aae994a725c44185
clairempr <clairempr@users.noreply.github.com>
2018-12-16 21:37:55 -0500

    Unit test for save_model in EmployeeAdmin

--- bureau/personnel/tests/test_admin.py
+++ bureau/personnel/tests/test_admin.py
@@ -1,13 +1,19 @@
 import string
 
 from django.contrib.admin import site
-from django.test import RequestFactory, TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client, RequestFactory, TestCase
 
+from military.tests.factories import RegimentFactory
 from places.tests.factories import RegionFactory
 
 from personnel.admin import EmployeeAdmin
 from personnel.models import Employee
 from personnel.tests.factories import EmployeeFactory
+
+
+User = get_user_model()
 
 
 class EmployeeAdminTestCase(TestCase):
@@ -34,6 +40,28 @@ class EmployeeAdminTestCase(TestCase):
                           'State in Employee.bureau_states should be in EmployeeAdmin.bureau_state')
         self.assertNotIn(state3.name, EmployeeAdmin.bureau_state(EmployeeAdmin, employee),
                          'State not in Employee.bureau_states should not be in EmployeeAdmin.bureau_state')
+
+
+    def test_save_model(self):
+        """
+        If Employee is a member of a VRC unit, 'vrc' should be True
+        """
+
+        # Set up superuser to log in to admin and create new Employee
+        User.objects.create_superuser('admin', 'admin@example.com', 'Password123')
+        self.client = Client()
+        self.client.login(username='admin', password='Password123')
+
+        # Oops, we're forgetting to set vrc to True, even though he's in a VRC unit! Hope save_model catches it...
+        self.client.post(
+            reverse('admin:personnel_employee_add'),
+            {'last_name': 'Dodge', 'first_name': 'Charles', 'gender': 'M', 'vrc': False,
+             'regiments': [RegimentFactory(vrc=True).id]},
+            follow=True,
+        )
+
+        self.assertTrue(Employee.objects.first().vrc,
+                "Employee in VRC unit should have 'vrc' set to true after saving")
 
 class FirstLetterListFilterTestCase(TestCase):
     """
