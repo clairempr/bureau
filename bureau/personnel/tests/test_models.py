f7acd6380f567644b66e8ad06d391543355537ce
clairempr <clairempr@users.noreply.github.com>
2019-11-10 20:10:06 -0500

    Unit tests for EmployeeManager birthplace_known and foreign_born

--- bureau/personnel/tests/test_models.py
+++ bureau/personnel/tests/test_models.py
@@ -1,8 +1,40 @@
 from django.test import TestCase
 
 from military.tests.factories import RegimentFactory
+from places.tests.factories import CountryFactory, PlaceFactory
 
+from personnel.models import Employee
 from personnel.tests.factories import EmployeeFactory
+
+
+class EmployeeManagerTestCase(TestCase):
+    """
+    Test EmployeeManager
+    """
+
+    def test_birthplace_known(self):
+        """
+        Should return employees with place_of_birth filled
+        """
+
+        self.assertIn(EmployeeFactory(place_of_birth=PlaceFactory()), Employee.objects.birthplace_known(),
+                      "Employee with place_of_birth filled should be in Employee.objects.birthplace_known()")
+        self.assertNotIn(EmployeeFactory(place_of_birth=None), Employee.objects.birthplace_known(),
+                      "Employee with place_of_birth empty shouldn't be in Employee.objects.birthplace_known()")
+
+    def test_foreign_born(self):
+        """
+        Should return employees with place_of_birth filled and country code not 'US'
+        """
+        self.assertNotIn(EmployeeFactory(place_of_birth=None), Employee.objects.foreign_born(),
+                      "Employee with place_of_birth empty shouldn't be in Employee.objects.foreign_born()")
+        self.assertNotIn(EmployeeFactory(place_of_birth=PlaceFactory(country=CountryFactory(code2='US'))),
+                         Employee.objects.foreign_born(),
+                         "Employee with place_of_birth in 'US' shouldn't be in Employee.objects.foreign_born()")
+        self.assertIn(EmployeeFactory(place_of_birth=PlaceFactory(country=CountryFactory(code2='DE'))),
+                      Employee.objects.foreign_born(),
+                      "Employee with place_of_birth in 'DE' should be in Employee.objects.foreign_born()")
+
 
 
 class EmployeeTestCase(TestCase):
@@ -12,7 +44,7 @@ class EmployeeTestCase(TestCase):
 
     def test_str(self):
         """
-        If __str__ should return Employee.last_name, Employee.first_name
+        __str__ should return Employee.last_name, Employee.first_name
         """
 
         employee = EmployeeFactory()
