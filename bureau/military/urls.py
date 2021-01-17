433906e6e4fd8bb517ad61d539756cf5a9e936ae
Claire <clairempr@users.noreply.github.com>
2021-01-16 21:06:10 -0500

    Regiment detail view and separate list view for VRC

--- bureau/military/urls.py
+++ bureau/military/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from bureau.military.views import (
+    regiment_detail_view,
     regiment_list_view,
+    vrc_regiment_list_view,
 )
 
 app_name = "military"
 urlpatterns = [
     path("", view=regiment_list_view, name="regiment_list"),
+    path("vrc/", view=vrc_regiment_list_view, name="vrc_regiment_list"),
+    path("<uuid:pk>/", view=regiment_detail_view, name="regiment_detail"),
 ]
--- bureau/military/views.py
+++ bureau/military/views.py
@@ -1,12 +1,35 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from military.models import Regiment
+
+class RegimentDetailView(DetailView):
+
+    model = Regiment
+
+
+regiment_detail_view = RegimentDetailView.as_view()
+
 
 class RegimentListView(ListView):
 
     model = Regiment
     slug_field = "name"
     slug_url_kwarg = "name"
+    regiment_type = 'all_regiments'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context[self.regiment_type] = True
+        return context
 
 
 regiment_list_view = RegimentListView.as_view()
+
+
+class VRCRegimentListView(RegimentListView):
+
+    queryset = Regiment.objects.filter(vrc=True)
+    regiment_type = 'vrc_regiments'
+
+
+vrc_regiment_list_view = VRCRegimentListView.as_view()
