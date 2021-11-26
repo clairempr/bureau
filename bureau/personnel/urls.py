508b97d2dfd8d64891cc9b2abb8c4375f82d7ce4
Claire <clairempr@users.noreply.github.com>
2021-11-26 11:14:02 -0500

    Add EmployeesWithAilmentListView

--- bureau/personnel/urls.py
+++ bureau/personnel/urls.py
@@ -3,10 +3,15 @@ from django.urls import path
 from bureau.personnel.views import (
     employee_detail_view,
     employees_born_resided_died_in_place_view,
+    employees_with_ailment_list_view,
 )
 
 app_name = "personnel"
 urlpatterns = [
+    path("ailment/<uuid:ailment>/", view=employees_with_ailment_list_view,
+         name="employees_with_ailment_list"),
+    path("ailment_type/<uuid:ailment_type>/", view=employees_with_ailment_list_view,
+         name="employees_with_ailment_type_list"),
     path("place/<uuid:place>/", view=employees_born_resided_died_in_place_view,
          name="employees_born_resided_died_in_place"),
     path("<uuid:pk>/", view=employee_detail_view, name="employee_detail"),
--- bureau/personnel/views.py
+++ bureau/personnel/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import DetailView, TemplateView
+from django.views.generic import DetailView, ListView, TemplateView
 
+from medical.models import Ailment, AilmentType
 from personnel.models import Employee
 from places.models import Place
 
@@ -44,3 +45,64 @@ class EmployeesBornResidedDiedInPlaceView(TemplateView):
 
 
 employees_born_resided_died_in_place_view = EmployeesBornResidedDiedInPlaceView.as_view()
+
+class EmployeesWithAilmentListView(ListView):
+    """
+    If ailment specified, list all employees with that ailment,
+    otherwise if ailment_type specified, list all employees with that ailment type
+    """
+
+    model = Employee
+    paginate_by = 25
+    queryset = Employee.objects.all()
+    template_name = "personnel/employees_with_ailment_list.html"
+
+    def get_ailment(self):
+        # If ailment is in kwargs and it's the pk of an Ailment, return the Ailment
+        # otherwise return None
+        ailment_pk = self.kwargs.get('ailment')
+        if ailment_pk:
+            try:
+                return Ailment.objects.get(pk=ailment_pk)
+            except Ailment.DoesNotExist:
+                pass
+
+        return None
+
+    def get_ailment_type(self):
+        # If ailment_type is in kwargs and it's the pk of an AilmentType, return the AilmentType
+        # otherwise return None
+        ailment_type_pk = self.kwargs.get('ailment_type')
+        if ailment_type_pk:
+            try:
+                return AilmentType.objects.get(pk=ailment_type_pk)
+            except AilmentType.DoesNotExist:
+                pass
+
+        return None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        ailment = self.get_ailment()
+
+        if ailment:
+            context['ailment'] = ailment
+        else:
+            context['ailment'] = self.get_ailment_type()
+
+        return context
+
+    def get_queryset(self):
+        ailment = self.get_ailment()
+        if ailment:
+            return self.queryset.filter(ailments=ailment)
+
+        ailment_type = self.get_ailment_type()
+        if ailment_type:
+            return self.queryset.filter(ailments__type=ailment_type)
+
+        return self.queryset
+
+
+employees_with_ailment_list_view = EmployeesWithAilmentListView.as_view()
