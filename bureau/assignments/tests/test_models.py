6b61497184ae0038cb6c65454c74954f9d042629
Claire <clairempr@users.noreply.github.com>
2021-12-16 14:19:03 -0500

    Unit test for AssignmentManager.in_place()

--- bureau/assignments/tests/test_models.py
+++ bureau/assignments/tests/test_models.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 
 from assignments.models import Assignment
 from assignments.tests.factories import AssignmentFactory, PositionFactory
+from places.tests.factories import CityFactory, CountyFactory, PlaceFactory, RegionFactory
 
 
 class AssignmentManagerTestCase(TestCase):
@@ -44,6 +45,58 @@ class AssignmentManagerTestCase(TestCase):
         self.assertIn(AssignmentFactory(start_date=PartialDate('1866'), end_date=PartialDate('1867')),
                       Assignment.objects.during_year(1866),
                       "Assignment that starts in and ends after year should be in Assignment.objects.during_year()")
+
+    def test_in_place(self):
+        """
+        in_place() should return assignments in a particular place, according to how specific the place is
+        """
+
+        # Set up places
+        alabama = PlaceFactory(city=None, county=None, region=RegionFactory(name='alabama'))
+        bacon_level = PlaceFactory(city=CityFactory(name='Bacon Level', region=alabama.region, country=alabama.country),
+                                   county=None)
+        randolph_county = PlaceFactory(city=None, county=CountyFactory(name='Randolph', state=alabama.region,
+                                                                       country=alabama.country))
+
+        # Set up assignments
+        assignment_in_alabama = AssignmentFactory()
+        assignment_in_alabama.places.add(alabama)
+        assignment_in_bacon_level = AssignmentFactory()
+        assignment_in_bacon_level.places.add(bacon_level)
+        assignment_in_randolph_county = AssignmentFactory()
+        assignment_in_randolph_county.places.add(randolph_county)
+
+        # If exact is True, only assignments in that exact place should be returned
+        self.assertIn(assignment_in_alabama, Assignment.objects.in_place(alabama, exact=True),
+                      'Assignment.objects.in_place(exact=True) for state should return assignments in that state')
+        for assignment in [assignment_in_bacon_level, assignment_in_randolph_county]:
+            self.assertNotIn(assignment, Assignment.objects.in_place(alabama, exact=True),
+                    "Assignment.objects.in_place(exact=True) for state shouldn't return assignments in city or county")
+        self.assertIn(assignment_in_bacon_level, Assignment.objects.in_place(bacon_level, exact=True),
+                      'Assignment.objects.in_place(exact=True) for city should return assignments in that city')
+        for assignment in [assignment_in_alabama, assignment_in_randolph_county]:
+            self.assertNotIn(assignment, Assignment.objects.in_place(bacon_level, exact=True),
+                        'Assignment.objects.in_place(exact=True) for city should only return assignments in city')
+        self.assertIn(assignment_in_randolph_county, Assignment.objects.in_place(randolph_county, exact=True),
+                      'Assignment.objects.in_place(exact=True) for county should return assignments in that county')
+        for assignment in [assignment_in_alabama, assignment_in_bacon_level]:
+            self.assertNotIn(assignment, Assignment.objects.in_place(randolph_county, exact=True),
+                        'Assignment.objects.in_place(exact=True) for county should only return assignments in county')
+
+        # If exact is False, assignments in that place and places in that place should be returned
+        for assignment in [assignment_in_alabama, assignment_in_bacon_level, assignment_in_randolph_county]:
+            self.assertIn(assignment, Assignment.objects.in_place(alabama, exact=False),
+                          'Assignment.objects.in_place(exact=False) for state should return all assignments in state')
+        self.assertIn(assignment_in_bacon_level, Assignment.objects.in_place(bacon_level, exact=False),
+                      'Assignment.objects.in_place(exact=False) for city should return assignments in that city')
+        for assignment in [assignment_in_alabama, assignment_in_randolph_county]:
+            self.assertNotIn(assignment, Assignment.objects.in_place(bacon_level, exact=False),
+                        'Assignment.objects.in_place(exact=False) for city should only return assignments in city')
+        self.assertIn(assignment_in_randolph_county, Assignment.objects.in_place(randolph_county, exact=False),
+                      'Assignment.objects.in_place(exact=False) for county should return assignments in that county')
+        for assignment in [assignment_in_alabama, assignment_in_bacon_level]:
+            self.assertNotIn(assignment, Assignment.objects.in_place(randolph_county, exact=False),
+                        'Assignment.objects.in_place(exact=False) for county should only return assignments in county')
 
 
 class PositionTestCase(TestCase):
