cd588cf61482538849ca4f30c58d5241721a08d9
Claire <clairempr@users.noreply.github.com>
2021-12-23 19:11:41 -0500

    Unit test for RegimentListView.get_queryset()

--- bureau/military/tests/test_views.py
+++ bureau/military/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
+from military.tests.factories import RegimentFactory
 from military.views import RegimentListView
 
 class RegimentListViewTestCase(TestCase):
@@ -33,3 +34,29 @@ class RegimentListViewTestCase(TestCase):
         response = self.client.get(reverse(self.url))
         self.assertEqual(response.context['search_text'], '',
                          "If 'search_text' wasn't supplied, it should be empty in context of RegimentListView")
+
+    def test_get_queryset(self):
+        """
+        If GET parameter 'search_text' is supplied, get_queryset() should filter the default queryset (all Regiments)
+        """
+
+        maine_regt = RegimentFactory(name='3rd Maine Infantry')
+        usct_regt = RegimentFactory(name='113th US Colored Infantry')
+        vrc_regt = RegimentFactory(name='24th Regiment, Veteran Reserve Corps')
+
+        # If no GET parameter 'search_text' supplied, get_queryset() should return default queryset (all Regiments)
+        request = RequestFactory().get('/')
+        view = RegimentListView(kwargs={}, object_list=[], request=request)
+        queryset = view.get_queryset()
+        self.assertSetEqual(set(queryset), {maine_regt, usct_regt, vrc_regt},
+                            "RegimentListView.get_queryset() should return all regiments if no 'search_text' supplied")
+
+        # If 'search_text' parameter supplied, get_queryset() should filter the default queryset
+        request = RequestFactory().get('/', {'search_text': 'Maine'})
+        view = RegimentListView(kwargs={}, object_list=[], request=request)
+        queryset = view.get_queryset()
+        self.assertIn(maine_regt, queryset,
+                      "RegimentListView.get_queryset() should return regiments whose names contain 'search_text'")
+        for regiment in [usct_regt, vrc_regt]:
+            self.assertNotIn(regiment, queryset,
+                      "RegimentListView.get_queryset() should return only regiments whose names contain 'search_text'")
