646dd2151c6938a63f0acccb91008fb4b55e9f80
Claire <clairempr@users.noreply.github.com>
2022-01-11 17:23:49 -0500

    Unit test for EmployeesWithAilmentListView template

---
+++ bureau/personnel/tests/test_templates.py
@@ -0,0 +1,50 @@
+from django.core.paginator import Paginator
+from django.template.loader import render_to_string
+from django.test import TestCase
+
+from medical.tests.factories import AilmentFactory, AilmentTypeFactory
+from personnel.tests.factories import EmployeeFactory
+
+class EmployeesWithAilmentListViewTemplateTestCase(TestCase):
+    """
+    Test template of personnel.views.EmployeesWithAilmentListView
+    """
+
+    def setUp(self):
+        self.template = 'personnel/employees_with_ailment_list.html'
+
+    def test_template(self):
+        page_header_no_employees = '<div class="page-header">Employees With {ailment}</div>'
+        page_header_with_employees = '<div class="page-header">Employees With {ailment} ({count})</div>'
+
+        ailment_type = AilmentTypeFactory(name='Headache')
+        ailment = AilmentFactory(name='Migraine', type=ailment_type)
+        employee = EmployeeFactory()
+
+        # "Employees With <Ailment>" should be in page header if 'ailment' is an Ailment and no employees found
+        context = {'ailment': ailment}
+        rendered = render_to_string(self.template, context)
+        page_header = page_header_no_employees.format(ailment=ailment)
+        self.assertInHTML(page_header, rendered,
+                          msg_prefix='Employees With <Ailment> should be in page header if no employees with ailment')
+
+        # "Employees With <AilmentType>" should be in html if 'ailment' is an AilmentType and no employees found
+        context = {'ailment': ailment_type}
+        rendered = render_to_string(self.template, context)
+        page_header = page_header_no_employees.format(ailment=ailment_type)
+        self.assertInHTML(page_header, rendered,
+                msg_prefix='Employees With <AilmentType> should be in page header if no employees with ailment type')
+
+        # "Employees With <Ailment> (count)" should be in html
+        paginator = Paginator(object_list=[employee], per_page=1)
+        context = {'ailment': ailment,
+                   'employee_list': paginator.object_list,
+                   'paginator': paginator}
+        rendered = render_to_string(self.template, context)
+        page_header = page_header_with_employees.format(ailment=ailment, count=len(paginator.object_list))
+        self.assertInHTML(page_header, rendered,
+                msg_prefix='Employees With <Ailment> (<count>) should be in page header if employees with ailment type')
+
+        # Employees should be listed
+        self.assertTrue(employee.first_name in rendered, 'Employees should be listed in {}'.format(self.template))
+        self.assertTrue(employee.last_name in rendered, 'Employees should be listed in {}'.format(self.template))
