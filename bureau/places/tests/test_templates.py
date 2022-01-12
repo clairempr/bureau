9bf7cfe3092fdaa4d421af5e889d97c5162d5ed9
Claire <clairempr@users.noreply.github.com>
2022-01-12 11:43:05 -0500

    Update BureauStateDetailView and add unit tests for that and StateComparisonView template

---
+++ bureau/places/tests/test_templates.py
@@ -0,0 +1,54 @@
+from django.template.loader import render_to_string
+from django.test import TestCase
+
+from personnel.tests.factories import EmployeeFactory
+from places.tests.factories import CityFactory, PlaceFactory, RegionFactory
+
+
+class BureauStateDetailViewTemplateTestCase(TestCase):
+    """
+    Test template of places.views.BureauStateDetailView
+    """
+
+    def setUp(self):
+        self.template = 'places/bureau_state_detail.html'
+        self.state = RegionFactory(name='Pelican State', bureau_operations=True)
+
+    def test_template(self):
+        page_header = '<div class="page-header">{state} - {employee_count} {employees}</div>'
+
+        # If no employees/stats/assignment places, employee count should be 0 and there should be
+        # "No statistics for this location" and "No assignments found"
+        context = {'object': self.state}
+        rendered = render_to_string(self.template, context)
+        expected_page_header = page_header.format(state=self.state, employee_count=0, employees='employees')
+        self.assertInHTML(expected_page_header, rendered)
+        for text in ['No statistics for this location', 'No assignments found']:
+            self.assertTrue(text in rendered,
+                "If no employees/assignments, BureauStateDetailView template should contain '{}'".format(text))
+
+        # One employee: employee count should be "1 employee"
+        employee1 = EmployeeFactory(last_name='Butts', first_name='Simeon')
+        employee1.bureau_states.add(self.state)
+        context = {'object': self.state}
+        rendered = render_to_string(self.template, context)
+        expected_page_header = page_header.format(state=self.state, employee_count=1, employees='employee')
+        self.assertInHTML(expected_page_header, rendered)
+
+        # Two employees: employee count should be "2 employees"
+        employee2 = EmployeeFactory(last_name='Bishop', first_name='William')
+        employee2.bureau_states.add(self.state)
+        rendered = render_to_string(self.template, context)
+        expected_page_header = page_header.format(state=self.state, employee_count=2, employees='employees')
+        self.assertInHTML(expected_page_header, rendered)
+
+        # Stats/assignment places: should be in html
+        assignment_place = PlaceFactory(city=CityFactory(name='Vernon', region=self.state))
+        context = {'object': self.state,
+                   'stats': [('% VRC', '50.0')],
+                   'assignment_places': [assignment_place]}
+        rendered = render_to_string(self.template, context)
+        self.assertTrue('% VRC' in rendered,
+                        'If stats in context, they should be shown in BureauStateDetailView template')
+        self.assertTrue(str(assignment_place) in rendered,
+                        'If assignment places in context, they should be shown in BureauStateDetailView template')
--- bureau/stats/tests/test_templates.py
+++ bureau/stats/tests/test_templates.py
@@ -1,9 +1,16 @@
+from django.db.models import Count, FloatField
+from django.db.models.functions import Cast
 from django.template.loader import render_to_string
 from django.test import TestCase
 
+from personnel.tests.factories import EmployeeFactory
+from places.models import Region
+from places.tests.factories import RegionFactory
+
+
 class StatisticsTemplateTestCase(TestCase):
     """
-    Test template of stats.Views.GeneralView
+    Test template of stats.views.GeneralView
     """
 
     def setUp(self):
@@ -18,3 +25,47 @@ class StatisticsTemplateTestCase(TestCase):
 
         rendered = render_to_string(self.template, context)
         self.assertInHTML('<title>Statistics</title>', rendered)
+
+
+class StateComparisonViewTemplateTestCase(TestCase):
+    """
+    Test template of stats.views.StateComparisonView
+    """
+
+    def setUp(self):
+        self.template = 'stats/state_comparison.html'
+
+        self.florida = RegionFactory(name='Florida')
+        self.south_carolina = RegionFactory(name='South Carolina')
+
+        self.florida_employee_count = 3
+        self.south_carolina_employee_count = 5
+
+        for i in range(self.florida_employee_count):
+            employee = EmployeeFactory()
+            employee.bureau_states.add(self.florida)
+
+        for i in range(self.south_carolina_employee_count):
+            employee = EmployeeFactory()
+            employee.bureau_states.add(self.south_carolina)
+
+    def test_template(self):
+        # Stats consist of labels and lists of top states for that stat, annotated with values
+        total_employees = Region.objects.annotate(
+            value=Cast(Count('employee_employed'), FloatField()))
+
+        context = {'stats': [('Employee count', total_employees)]}
+        rendered = render_to_string(self.template, context)
+
+        # Title should be in html
+        self.assertInHTML('<title>State Comparison</title>', rendered)
+
+        # Stats label should be in html
+        self.assertTrue('Employee count' in rendered, 'Stats label should be shown in StateComparisonView template')
+
+        # State names/employee counts should be in html
+        for state in [self.florida, self.south_carolina]:
+            self.assertTrue(state.name in rendered)
+
+        for employee_count in [self.florida_employee_count, self.south_carolina_employee_count]:
+            self.assertTrue(str(employee_count) in rendered)
