163a1890f82f31aabca15359dd44a1733c0dcc70
Claire <clairempr@users.noreply.github.com>
2022-01-22 09:48:50 -0500

    Remove SECURE_CONTENT_TYPE_NOSNIFF from settings because it already defaults to True with Django 3.0

--- config/settings/production.py
+++ config/settings/production.py
@@ -47,8 +47,6 @@ SECURE_HSTS_SECONDS = 60
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool('DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
 SECURE_HSTS_PRELOAD = env.bool('DJANGO_SECURE_HSTS_PRELOAD', default=True)
-# https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
-SECURE_CONTENT_TYPE_NOSNIFF = env.bool('DJANGO_SECURE_CONTENT_TYPE_NOSNIFF', default=True)
 
 # STORAGES
 # ------------------------------------------------------------------------------
