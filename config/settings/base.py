76b34e0e46e14d594c3dc944244e2bb6a787b083
Claire <clairempr@users.noreply.github.com>
2022-02-06 13:02:19 -0500

    Add DEFAULT_AUTO_FIELD to settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,6 +45,8 @@ DATABASES = {
     "default": env.db("DATABASE_URL"),
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
+# https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # URLS
 # ------------------------------------------------------------------------------
