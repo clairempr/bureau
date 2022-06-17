6a128fa8659295b7a6e5344ee28b0ddf0a61d2f7
Claire <clairempr@users.noreply.github.com>
2022-06-16 22:24:47 -0400

    Speed up Assignment admin list loading with prefetch_related

--- bureau/assignments/admin.py
+++ bureau/assignments/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Assignment, Position
 
+
 class AssignmentAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'employee')
     list_filter = ('positions', )
@@ -9,10 +10,17 @@ class AssignmentAdmin(admin.ModelAdmin):
     raw_id_fields = ('places', )
     save_on_top = True
 
+    def get_queryset(self, request):
+        qs = super(AssignmentAdmin, self).get_queryset(request)
+        qs = qs.prefetch_related('positions').prefetch_related('places')
+        return qs
+
+
 admin.site.register(Assignment, AssignmentAdmin)
 
 
 class PositionAdmin(admin.ModelAdmin):
     ordering = ('name',)
+
 
 admin.site.register(Position)
